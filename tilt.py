0a7ca5b184a095ce395cc6b1e8bb4a2f65edc472
dung <noajbarker@icloud.com>
2024-06-07 19:21:16 +0100

    pseudocoded what the controller is going for

--- tilt.py
+++ tilt.py
@@ -1,19 +1,20 @@
-import serial
+"""
+# WARNING!
+This is basically pseudocode so completely untested and not to be used elsewhere
+"""
 
+import serial
 ser = serial.Serial('COM3', 115200)
 
-def get_tilt_data():
+class MBInput:
     """
-    Reads the serial data from the micro:bit and returns the tilt data as a 
-    tuple of integers. Expects accelerations data in the form 'ax ay az'.
+    A micro:bit transfers a dictionary of useful data over serial to be used in
+    other programs as realtime input i.e. gyro controls. This object recieves
+    that data and parses it for easier use.
     """
+    
+    def get_gyro_data():
+        """Returns a tuple of acceleration in x, y, z directions."""
+        line = ser.readline().decode('utf-8').strip()
+        return (int(line[i]) for i in ["gyro_x", "gyro_y", "gyro_z"])
 
-    # have to read 2 lines to reduce delay... for some reason.
-    _    = ser.readline().decode('utf-8').strip() 
-    line = ser.readline().decode('utf-8').strip()
-
-    try:
-        ax, ay, az = line.split()
-        return int(ax), int(ay), int(az)
-    except ValueError:
-        raise ValueError('Expected string of <SPACE> separated floats, got: "' + line + '"')
